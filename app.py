c97e1976f2c360d3be5c9518f355f952c6ac3754
[khodifadkuldip] <[khodifadkuldip87@gmail.com]>
2026-07-14 12:11:15 +0530

    developer story

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ def addition(a, b):
 
 result=addition(5, 3)
 print("The result of addition is:", result)
+
+def subtraction(a, b):
+    return a - b
+
+result=subtraction(5, 3)
+print("The result of subtraction is:", result)
